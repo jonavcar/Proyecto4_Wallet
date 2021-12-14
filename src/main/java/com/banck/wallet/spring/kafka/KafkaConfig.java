6a35ebc7be1445663b9f2f114f6b44990941c323
Joaquin Navarro <jonavcar@gmail.com>
2021-12-14 10:02:44 -0500

    Wallet

--- src/main/java/com/banck/wallet/spring/kafka/KafkaConfig.java
+++ src/main/java/com/banck/wallet/spring/kafka/KafkaConfig.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
@@ -23,6 +24,9 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 @Configuration
 public class KafkaConfig {
 
+    @Value(value = "${kafka.bootstrapAddress}")
+    private String bootstrapAddress;
+
     @Bean
     public KafkaTemplate<String, Wallet> kafkaTemplate() {
         return new KafkaTemplate<>(producerFactory());
@@ -31,7 +35,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, Wallet> producerFactory() {
         Map<String, Object> map = new HashMap<>();
-        map.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        map.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
         map.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         map.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(map);
